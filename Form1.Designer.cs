00a17a9880df75e60ec644f34725e1ebd9e06606
katarzynadobro <86267298+katarzynadobro@users.noreply.github.com>
2021-06-24 14:09:37 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Hangman_game
+{
+    partial class frmHangman
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Guess = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hangman_game.Properties.Resources._1;
+            this.pictureBox2.Location = new System.Drawing.Point(257, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(235, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Guess
+            // 
+            this.Guess.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Guess.Location = new System.Drawing.Point(137, 352);
+            this.Guess.Name = "Guess";
+            this.Guess.Size = new System.Drawing.Size(54, 46);
+            this.Guess.TabIndex = 2;
+            this.Guess.Text = "a";
+            this.Guess.UseVisualStyleBackColor = true;
+            this.Guess.Click += new System.EventHandler(this.Guess_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(234, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "b";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Guess_Click);
+            // 
+            // frmHangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Guess);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "frmHangman";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Guess;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman_game
+{
+    public partial class frmHangman : Form
+    {
+        private Bitmap[] hangImages = {Hangman_game.Properties.Resources.1, Hangman_game.Properties.Resources.2, Hangman_game.Properties.Resources.3, Hangman_game.Properties.Resources.4, Hangman_game.Properties.Resources.5, Hangman_game.Properties.Resources.6, Hangman_game.Properties.Resources.7 };
+        private int wrongGuesses = 0;
+        public frmHangman()
+        {
+            InitializeComponent();
+        }
+
+        private void Guess_Click(object sender, EventArgs e)
+        {
+            wrongGuesses++;
+            pictureBox2.Image = handImages[wrongGuesses]
+        }
+    }
+}
